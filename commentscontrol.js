e0be5d239c3ccaac8d19e0eab9f27b71b296bad3
Sircule14 <victor.jromar@gmail.com>
2023-10-24 12:22:14 -0600

    comment and explain the function

--- commentscontrol.js
+++ commentscontrol.js
@@ -2,14 +2,14 @@
 
 // Calculate the total of comments so far
 
-const comments = [10,11,9,8,10,5];
+const comments = [10,11,9,8,10,5]; //Array that will contain the number of comments
 
-function totalCommentsCount(comments){
-    let sum = 0;
-    for (let i = 0; i < comments.length; i++){
-        sum += comments[i];
-    }
-    return sum;
+function totalCommentsCount(comments){//The function recives an argument (comments) 
+    let sum = 0; // sum will be the total of comments
+    for (let i = 0; i < comments.length; i++){ //for loop, i declarated for iterate, while i will be minor than comments length add 1 to i (for iterate)
+        sum += comments[i]; //add to sum the valor that indicates de iterative (i) in comments array
+    }// for every iteration the value changes, first 10, next 11 until end (comments.length is for this) and add that value to sum
+    return sum; //finally return sum 
 }
 
-console.log(totalCommentsCount(comments));
+console.log(totalCommentsCount(comments));//console log for confirms that function works
