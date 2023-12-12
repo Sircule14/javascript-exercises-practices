3afb8b5ced264e1b3a63ad90685624e543dcabc9
Sircule14 <victor.jromar@gmail.com>
2023-12-11 19:42:27 -0700

    Function that sum two numbers of two arrays

---
+++ function1.js
@@ -0,0 +1,13 @@
+//Function that sum two arrays and return the result
+
+//Setup 
+
+array1 = [1,2,3];
+array2 = [10];
+
+function sumArrays(array,array2) {
+    return Number(array1.join('')) + Number(array2.join(''));
+}
+
+console.log(sumArrays(array1,array2))//Print 133 in the terminal
+//123 + 10
