15672718a03f8498365868a94d951d69a49c37b4
Sircule14 <victor.jromar@gmail.com>
2023-12-14 12:45:21 -0700

    add 3th function

--- Index.md
+++ Index.md
@@ -2,3 +2,8 @@
 Function that sum two arrays.
 First join the separate numbers.
 Finally sum the two arrays. 
+
+-> Function2: 
+Function for know if a given numbers is a palindrome
+
+-> Function3
---
+++ function3.js
@@ -0,0 +1,18 @@
+//Function to know the remnaining time of something
+
+function timeTo (remnaining) {
+    if (remnaining === 0) {
+      return 'Ready';
+    } else if (remnaining > 0) {
+    return 'not done';
+} else {
+    'You forgot to set timer.'
+}
+}
+
+//Test remaining time
+console.log(timeTo(0));
+//Test with 5 minutes
+console.log(timeTo(4))
+//Test if dont pass parameter to the function
+console.log(timeTo(""))
