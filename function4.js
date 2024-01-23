238c2bf2d42ed106dfdf05cc029b5d394669ba9e
Sircule14 <victor.jromar@gmail.com>
2024-01-22 20:32:23 -0700

    add description to function 4

--- function4.js
+++ function4.js
@@ -1,10 +1,12 @@
 //Factorizar un numero
 
 function factor(num) {
-    if(num === 0 || num === 1)
+    if(num === 0 || num === 1) //Si num equivale a 0 y a 1 el factorial regresa 1
     return 1;
+    //Inicia bucle for con uno menos de cada numero proporcionado
+    //Decrementa i una vez despues de cada iteracion.
   for (var i = num - 1; i >= 1; i--) {
-    num *=i
+    num *=i //Guardamos el valor de num en cada iteracion
   }
   return num;
 }
